27ab0bb36e8cb7e52582f05083c21b572154a2ad
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2024-12-20 16:26:15 +0530

    scroll fixed

--- src/applayout.tsx
+++ src/applayout.tsx
@@ -6,7 +6,7 @@ import { menuContext } from "./contexts/menu";
 function AppLayout() {
     const {state} = useContext(menuContext);
   return (
-    <div className="flex flex-col md:flex-row ">
+    <div className="flex flex-col md:flex-row overflow-scroll">
         <Sidebar />
         <div className={`absolute w-full md:relative md:w-[88%] md:top-0 top-[5rem] ${state && "md:blur-none blur-sm"}`}><Outlet /></div>
     </div>
